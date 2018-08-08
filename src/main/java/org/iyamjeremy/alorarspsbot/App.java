683020738096962451838ab2b6b03e736d2ad0ab
i-yam-jeremy <i-yam-jeremy@users.noreply.github.com>
2018-08-07 21:12:24 -0700

    Added bundled dependency javassist

--- src/main/java/org/iyamjeremy/alorarspsbot/App.java
+++ src/main/java/org/iyamjeremy/alorarspsbot/App.java
@@ -1,13 +1,20 @@
 package org.iyamjeremy.alorarspsbot;
 
+import javassist.*;
+
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main(String[] args) {
+	try {
+		ClassPool pool = ClassPool.getDefault();
+		CtClass cc = pool.get("java.awt.Rectangle");
+	} catch (javassist.NotFoundException e) {
+		
+	}
         System.out.println( "Hello World!" );
     }
 }
