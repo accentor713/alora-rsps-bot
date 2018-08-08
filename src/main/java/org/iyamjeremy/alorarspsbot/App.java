113b1a3e5541147d7c0bc4cee66f414129948411
i-yam-jeremy <i-yam-jeremy@users.noreply.github.com>
2018-08-07 17:42:29 -0700

    Setup maven project

---
+++ src/main/java/org/iyamjeremy/alorarspsbot/App.java
@@ -0,0 +1,13 @@
+package org.iyamjeremy.alorarspsbot;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
